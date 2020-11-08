732723ad6c36b825acba30e3a66e6e3b3a930cfb
Alexander Ivanov <oz.sasha.ivanov@gmail.com>
2020-11-08 20:42:29 +0300

    Default value for email and ssl

--- simple_feed/settings.py
+++ simple_feed/settings.py
@@ -146,7 +146,7 @@ EMAIL_HOST = config('EMAIL_HOST')
 EMAIL_PORT = config('EMAIL_PORT', cast=int)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-EMAIL_USE_SSL = config('EMAIL_USE_SSL', cast=bool)
+EMAIL_USE_SSL = config('EMAIL_USE_SSL', cast=bool, default=True)
 DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
 
 ACCOUNT_ACTIVATION_DAYS = config('ACCOUNT_ACTIVATION_DAYS', cast=int, default=1)
